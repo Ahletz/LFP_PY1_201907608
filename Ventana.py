2c1f81637d96d2dea50a208d9909aaf7e94297e3
Ahletz <85268562+Ahletz@users.noreply.github.com>
2022-03-09 12:49:21 -0600

    Interfaz grafica Principal v1

--- Ventana.py
+++ Ventana.py
@@ -1,4 +1,4 @@
-import tkinter 
+from tkinter import *
 
 class Grafica:
 
@@ -8,13 +8,20 @@ class Grafica:
     
     def Ventana(self):
 
-        ventana = tkinter.Tk()
+        ventana = Tk()
 
-        ventana.geometry('500x500')
-        ventana.configure(background='dark orchid')
+        lista = ['Reporte de Tokens','Reporte de Erroes','Manual de usuario', 'Manueal Tecnico']
 
-        print ('nuevo cambio')
-
+        ventana.geometry('900x600') #ventana 
+        ventana.configure(background='dark orchid') #color de la ventana 
+        boton1 = Button(text='Abrir Archivo').place(x=10,y=10) #boton de carga de archivos
+        boton2 = Button(text='Analizar Archivo').place(x=200,y=10)
+        area_texto= Text(ventana,height=30,width=110).place(x=10,y=60)
+        bar = StringVar(ventana) #coponenetes para lista menu 
+        bar.set('Reporte de Tokens') #Seleccion por defecto 
+        menu = OptionMenu(ventana, bar,*lista).place(x=500,y=10) #Menu generado en lista en la interfaz
+        boton3 = Button(text='Gemerar').place(x=650,y=12)
+        
 
         ventana.mainloop()
 
